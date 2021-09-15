74219b94d0d116463c0671cd5706a46ca2e4b860
kenriortega <kenriortega@gmail.com>
2021-09-15 11:13:11 -0400

    mod6: basic enums

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,44 @@
-// struct
+// enums
 
 struct User {
     name: String,
     email: String,
     age: i32,
     status: bool,
+    user_role: UserRole,
+    website: Website,
 }
 
-impl User {
-    fn age(&self) -> i32 {
-        return self.age;
-    }
-    fn email(&self) -> String {
-        return self.email.to_string();
-    }
+enum UserRole {
+    BASIC,
+    ADMIN,
 }
+
+enum Website {
+    URL(String),
+    INSTAGRAM(String),
+    LINKEDIN(String),
+    FACEBOOK(String),
+}
+
 fn main() {
     let mut user = User {
         name: "k".to_string(),
         email: String::from("k@mail.com"),
         age: 30,
         status: true,
+        user_role: UserRole::BASIC,
+        website: Website::INSTAGRAM("Instagram".to_string()),
     };
     user.status = false;
     println!("User {}", user.name);
-    let user2 = new_user(String::from("jo"), String::from("jo@mail.com"));
-    println!("User2 {}", user2.age);
 
-    let user1 = User {
-        name: "kas".to_string(),
-        email: "as@mai.com".to_string(),
-        ..user
-    };
-    println!("User {}", user1.name);
-
-    // tuples struct
-    struct Point(i32, i32, i32);
-    let p = Point(23, 3, 4);
+    let access = hasAccess(user.user_role);
 }
 
-fn new_user(name: String, email: String) -> User {
-    return User {
-        name,
-        email,
-        age: 30,
-        status: true,
-    };
+fn hasAccess(user_role: UserRole) -> bool {
+    match user_role {
+        UserRole::ADMIN => true,
+        UserRole::BASIC => false,
+    }
 }
