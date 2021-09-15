6beb08ea8a7d951e76c08ee88a2b94a82f28b445
kenriortega <kenriortega@gmail.com>
2021-09-15 13:00:30 -0400

    mod6: traits

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,78 @@
 #[allow(dead_code)] // esto es para q no alert por codigo no usado
 
-// generics
+// traits == rasgo en espannol
 fn main() {
-    let pt1 = Point { x: 0, y: 12 };
-    let pt2 = Point { x: 0, y: 12 };
-    calc_area(pt1, pt2)
+    let k = Humman;
+    k.say_hello();
+    let pelusa = Cat;
+    pelusa.say_hello();
+
+    println!("{}", Humman::run());
+
+    let age = Some(3);
+    println!("{}", age.is_gt_age());
+
+    // macros son funciones de rust q escriben codigo rust
+    // macro format y println
+    let u = User {
+        name: "L".to_string(),
+        age: 18,
+    };
+
+    println!("{}", u);
 }
 
-struct Point<T> {
-    x: T,
-    y: T,
+impl std::fmt::Display for User {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{} ({})", self.name, self.age)
+    }
+}
+// impl std::fmt::Debug for User {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         f.debug_struct("User")
+//             .field("name", &self.name)
+//             .field("age", &self.age)
+//             .finish()
+//     }
+// }
+// #[derive(Debug)]
+struct User {
+    name: String,
+    age: i32,
+}
+struct Humman;
+struct Cat;
+
+trait Speak {
+    fn say_hello(&self) -> String;
+    // al no recivir slef como parametro se considera static
+    // por lo que no es obligatorio impl
+    fn run() -> String {
+        "run".to_string()
+    }
 }
 
-fn calc_area<T>(pt1: Point<T>, pt2: Point<T>) {}
+impl Speak for Humman {
+    fn say_hello(&self) -> String {
+        " todo!()".to_string()
+    }
+}
+
+impl Speak for Cat {
+    fn say_hello(&self) -> String {
+        "miau".to_string()
+    }
+}
+
+trait DriverLicence {
+    fn is_gt_age(&self) -> bool;
+}
+
+impl DriverLicence for Option<i32> {
+    fn is_gt_age(&self) -> bool {
+        match self {
+            Some(age) => age > &18,
+            None => false,
+        }
+    }
+}
