0ddffc124cec3a2ec782ef86357201eabb67547e
kenriortega <kenriortega@gmail.com>
2021-09-19 15:19:21 -0400

    errors

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,26 @@
-use std::{ops::Deref, rc::Rc};
-
+use std::fs::File;
+use std::io::ErrorKind;
 #[allow(dead_code)] // esto es para q no alert por codigo no usado
 
 fn main() {
-    // referencias (&)
-    // let x = 5;
-    // let y = &x;
-    // Reference counter (contador de referencias)
-    // String y el Vec<T>
+    // manejo de errores
+    // Recuperables e.g: abrir archivos donde el path es incorrecto
+    // no hay exceptions
+    // Result<T,E>
+    let file = File::open("./main.rs");
+    match file {
+        Ok(file) => read_file(file),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => println!("NotFound"),
+            other_error => println!("Other error"),
+        },
+    }
 
-    // Los smart pointers son usualmente implementados usando structs,
-    // pero implementando los traits Deref y Drop
-    // Deref permite a las instacias de Smart pointers comportarse como referencias
-    // para que el mismo codigo q funciona con referencias, funcione con smart pointers
-
-    // Drop trait permite definir logica que se ejecute una vez que
-    // el smart pointer sale del scope
-
-    // Box<T = caja
-    // sirve para definir cuanta memoria necesita en el stack
-    // estando en el heap
-    // heap
-    // some_box()
-
-    // Reference Counted Smart Pointer: permite que un valor tenga muchos dueños
-    // Usamos Rc cuando queremos asignar datos en el heap para que sea accedido
-    // en multiples partes del código, y no podemos determinar en tiempo de
-    // compilación el último que accederá estos datos. Si supieramos de antemano
-    //  quien sería el último, podríamos hacer que ese último sea el dueño,
-    // pero no lo sabemos. Entonces Rc lleva un contador de referencias con
-    // todos los dueños, y cuando ya no quedan más dueños, puede limpiar el dato.
+    let fi2 = File::open("./main.rs").unwrap();
+    let fi2 = File::open("./main.rs").expect("NotFound");
+    // No-Recuperables e.g: tratar de acceder a un arreglo mas alla de su limite
+    // tiene un macro llamado panic!()
+    // panic!("explota")
 }
 
-fn some_defer() {
-    // hace posible la deferenciacion (*)
-    let x = 5;
-    let y = &x;
-    let z = &y;
-
-    if x == 5 {
-        println!("{}", 5);
-    }
-
-    if *y == 5 {
-        println!("{}", 5);
-    }
-
-    if **z == 5 {
-        println!("{}", 5);
-    }
-
-    let mb = MyBox::new(x);
-    if *mb == 5 {
-        println!("{}", 5);
-    }
-}
-
-struct MyBox<T>(T);
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
-}
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl<T> Drop for MyBox<T> {
-    fn drop(&mut self) {
-        println!("Drop")
-    }
-}
-fn some_box() {
-    let x = 12;
-    let y = Box::new(2);
-    println!("{}", y);
-
-    // linked list
-    // (v,nodo1) -> (v,nodo2)
-
-    enum List {
-        Node(i32, Rc<List>),
-        None,
-    }
-    let node3 = List::Node(100, Rc::new(List::None));
-    let node2 = List::Node(150, Rc::new(node3));
-    let node2_rc = Rc::new(node2);
-    let node1 = List::Node(10, Rc::clone(&node2_rc)); // deep clone
-    let node0 = List::Node(130, Rc::clone(&node2_rc));
-}
+fn read_file(file: File) {}
